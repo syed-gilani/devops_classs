9dce3dedb39066c54bc5ba6de04b3967c375cf5e
FA <fa.emessages@gmail.com>
2024-03-23 12:51:54 -0400

    test new sshkey

---
+++ misc.py
@@ -0,0 +1,27 @@
+mosque = 'ISG';
+
+# //a program to take input and check if palyndrom
+# def isPalindrome(s):
+#     return s == s[::-1]
+
+# s = "computer"
+# ans = isPalindrome(s)
+ 
+# if ans:
+#     print("Yes")
+# else:
+#     print("No")
+    
+# name  = input("enter a word: ")
+# if name == name[::-1]:
+#     print(f'{name} is a palindrome')
+# else:
+#     print(f'{name} is not a palindrome')
+
+
+#lists
+numbers = [1,2,3,4,5,5]
+numbers [1:3]
+print(numbers[1:3])
+print(max(numbers))
+
